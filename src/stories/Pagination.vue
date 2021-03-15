54788715121b85ecdbe603ab3ca7b0548578ab19
문선혜 <sun0916@danawa.com>
2021-03-15 16:11:30 +0900

    현제 페이지 클래스 추가

--- src/stories/Pagination.vue
+++ src/stories/Pagination.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="common_paginate">
     <div class="page_nav_area"><span class="nav_edge nav_edge_prev nav_edge_off"><span class="point_arw_l"><em class="txt_hide">이전 보기</em></span></span>
-    <div class="nums_area" v-for="number in perPage" :key="number"><span class="page_num <!--now_page-->">{{ number }}</span></div>
+    <div class="nums_area" v-for="number in perPage" :key="number"><span v-bind:class="[ {'page_num': true }, {'now_page': currentPage == number } ]">{{ number }}</span></div>
     <span class="nav_edge nav_edge_next nav_edge_off"><span class="point_arw_r"><em class="txt_hide">다음 보기</em></span></span></div>
     </div>
 </template>
